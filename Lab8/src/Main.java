e88f43b68cda512ddabdccbea8c08969e3f13c4e
maciekb05 <maciej.bielech@gmail.com>
2017-11-19 12:45:15 +0100

    Testing commit 2

--- Lab8/src/Main.java
+++ Lab8/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GIT second change2");
+        System.out.println("Hello GIT second change22");
     }
 }
