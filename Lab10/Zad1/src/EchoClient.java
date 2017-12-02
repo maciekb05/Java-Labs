64f478f19e253d37e428c936b81df6061dfee305
maciekb05 <maciej.bielech@gmail.com>
2017-12-02 11:54:08 +0100

    Working EchoServer and EchoClient

---
+++ Lab10/Zad1/src/EchoClient.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        String hostName = "localhost";
+        int portNumber = 3002;
+
+        try {
+            //Creating a socket
+            Socket echoSocket = new Socket(hostName, portNumber);
+            //Creating PrintWriter to send data to socket
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            //Creating BufferedReader to get data from the socket
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            //Standard input BufferedReader
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println(in.readLine());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Lab10/Zad1/src/EchoServer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        int portNumber = 3002;
+
+        try {
+            //Creating a serverSocket
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            //Accepting client socket
+            Socket clientSocket = serverSocket.accept();
+            //Creating PrintWriter to send data to client
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            //Creating BufferedReader to get data from client
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println("echo: " + inputLine);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
